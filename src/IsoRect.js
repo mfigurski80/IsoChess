0f813fd7880552650611c3f9016a743a055db16b
Mikolaj Figurski <meek.f80@gmail.com>
2020-07-06 09:46:47 -0400

    Add depth to isorect

--- src/IsoRect.js
+++ src/IsoRect.js
@@ -7,16 +7,25 @@ require('@danzen/createjs')
  * @param {int} scale - Scale of the rectangle (usually 25)     
  * @param {str} color - CSS color of the rectangle
  */
-const IsoRect = (center, scale, color) => {
+const IsoRect = (center, scale, fillColor, strokeColor) => {
     const r = new createjs.Shape()
     r.graphics
-        .beginFill(color)
-        .moveTo(center.x, center.y + .5 * scale)
-        .lineTo(center.x + 1 * scale, center.y)
-        .lineTo(center.x, center.y - .5 * scale)
-        .lineTo(center.x - 1 * scale, center.y)
-        .lineTo(center.x, center.y + .5 * scale)
+        .beginFill(fillColor)
+        .beginStroke(strokeColor)
+        .moveTo(center.x + 1 * scale, center.y)     // right
+        .lineTo(center.x + 1 * scale, center.y + .2 * scale)    // bottom right
+        .lineTo(center.x, center.y + .7 * scale)                // bottom bottom
+        .lineTo(center.x - 1 * scale, center.y + .2 * scale)    // bottom left
+        .lineTo(center.x - 1 * scale, center.y)    // left
+        .lineTo(center.x, center.y - .5 * scale)    // top
+        .lineTo(center.x + 1 * scale, center.y)     // right
+        .lineTo(center.x, center.y + .5 * scale)    // bottom
+        .lineTo(center.x - 1 * scale, center.y)     // left
         .endStroke()
+
+    // r.graphics
+    //     .beginFill(color)
+    //     .moveTo(center.x, center.y)
     return r
 }
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ canvas.height = window.innerHeight;
 
 const stage = new createjs.Stage(canvas)
 
-const rect = IsoRect(Point(100, 100), 25, '#EEE')
-stage.addChild(rect);
+const rect1 = IsoRect(Point(100, 100), 60, '#555', '#EEE')
+const rect2 = IsoRect(Point(160, 130), 60, '#EEE', '#555')
+const rect3 = IsoRect(Point(220, 160), 60, '#555', '#EEE')
+
+stage.addChild(rect1)
+stage.addChild(rect2)
+stage.addChild(rect3)
+
 stage.update()
